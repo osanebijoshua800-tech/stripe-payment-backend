d139b3f8d3fb72e669d19fece9cfd6342dd504c8
unknown <osanebijoshua800@gmail.com>
2026-07-04 21:57:56 +0100

    Configure production scripts for deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+
 // Initialize Stripe with your Secret Key from the .env file
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
